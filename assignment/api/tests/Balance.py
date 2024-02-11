e54b815eb9d048aecbf3b47ccb9401551addf28a
Israel F Prates <israel.f.prates@gmail.com>
2024-02-11 20:57:11 -0300

    Add balance tests.

---
+++ assignment/api/tests/Balance.py
@@ -0,0 +1,19 @@
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class BalanceTests(APITestCase):
+    deposit_data = { 'type' : 'deposit', 'destination' : '100', 'amount' : 100 }
+
+    def test_existing_account(self):
+        """Balance must be returned for existing account."""
+        response = self.client.post('/event', self.deposit_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.get(f'/balance?account_id=100')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), 100)
+
+    def test_non_existing_account(self):
+        """Balance should return zero and HTTP 404 for non-existing account."""
+        response = self.client.get(f'/balance?account_id=100')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.json(), 0)
