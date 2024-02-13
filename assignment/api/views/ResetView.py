6b1c2714f10a997c0d51994f715ad1d9fee48783
Israel F Prates <israel.f.prates@gmail.com>
2024-02-13 00:24:51 -0300

    Render OK message as expected by tests.

--- assignment/api/views/ResetView.py
+++ assignment/api/views/ResetView.py
@@ -1,9 +1,9 @@
 from api.models.Account import Account
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse
 
 class ResetView(APIView):
     def post(self, request):
         Account.objects.all().delete()
-        return Response(status=status.HTTP_200_OK)
+        return HttpResponse('OK', status=status.HTTP_200_OK)
