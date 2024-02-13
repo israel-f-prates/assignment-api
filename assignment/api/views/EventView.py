b1eaa3c41e2910e76d28931f987c44c9a8093481
Israel F Prates <israel.f.prates@gmail.com>
2024-02-13 01:19:25 -0300

    Add comments on some key portions of processing.

--- assignment/api/views/EventView.py
+++ assignment/api/views/EventView.py
@@ -28,12 +28,32 @@ class EventView(APIView):
         try:
             origin.withdraw(amount)
         except ValueError:
+            # By returning 422 instead of a less descriptive code, I could be
+            # leaking too much information to the client (i.e.: a user could
+            # infer the balance by trial and error).
             return Response(0, status.HTTP_422_UNPROCESSABLE_ENTITY)
         origin.save()
         origin_serializer = AccountSerializer(origin)
         withdraw_serializer = WithdrawSerializer({ 'origin' : origin_serializer.data })
         return Response(withdraw_serializer.data, status.HTTP_201_CREATED)
 
+    # Since this operation relies on first withdrawing and then depositing,
+    # it is not suited for actual use, because it is prone to racing conditions
+    # or could leave an inconsistent state in the event that the application
+    # fails in-between the operations.
+    #
+    # Since I don't know on which platform this code would run, I cannot blindly
+    # rely on a serialization mechanism (for example: if this would be served as
+    # a multiprocess app, I would need an OS semaphore, but the implementation
+    # differs depending on the platform; if it were multithreaded, then I could
+    # rely on the facilities provided by Python alone).
+    #
+    # On the other hand, I could delegate the serialization to the database by
+    # creating a stored procedure that uses atomic transactions, but I won't go
+    # down this route for two reasons:
+    #   1. This is a sample app and should be kept simple.
+    #   2. The database technology I'm using is far from adequate to be used
+    #      for such purposes.
     def transfer(self, origin_id, destination_id, amount):
         try:
             origin = Account.objects.get(id=origin_id)
@@ -42,6 +62,7 @@ class EventView(APIView):
         try:
             origin.withdraw(amount)
         except ValueError:
+            # Same as earlier comment about HTTP 422.
             return Response(0, status.HTTP_422_UNPROCESSABLE_ENTITY)
         origin.save()
         destination, _ = Account.objects.get_or_create(id=destination_id)
