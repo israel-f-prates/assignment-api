fe025bf648b9ff2c6b7b6cba41baae9d8716276e
Israel F Prates <israel.f.prates@gmail.com>
2024-02-13 00:16:57 -0300

    Expose balance as a property to handle floating point.

--- assignment/api/models/Account.py
+++ assignment/api/models/Account.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Account(models.Model):
     id = models.CharField(primary_key=True, max_length=128, unique=True)
-    balance = models.DecimalField(max_digits=17, decimal_places=2, default=0)
+    _balance = models.DecimalField(max_digits=17, decimal_places=2, default=0)
 
     def deposit(self, amount):
         amount = Decimal(amount)
@@ -14,3 +14,14 @@ class Account(models.Model):
         amount = Decimal(amount)
         if amount > 0 and amount <= self.balance:
             self.balance -= amount
+
+    @property
+    def balance(self):
+        if self._balance % 1 == 0:
+            return int(self._balance)
+        else:
+            return self._balance
+
+    @balance.setter
+    def balance(self, value):
+        self._balance = value
